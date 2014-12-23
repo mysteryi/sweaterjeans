2222524cbb91fd9ddde257dc4c1f8a5e956b6153
Simon Yi <yi.simon.k@gmail.com>
2014-12-23 17:24:01 -0500

    adding attr_accessor lines on pin.rb model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
      validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+     attr_accessor :photo_file_name
+     attr_accessor :photo_content_type
+     attr_accessor :photo_file_size
+     attr_accessor :photo_updated_at
 end
