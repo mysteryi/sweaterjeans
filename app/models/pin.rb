723b21e08c8f01746700ed95747912706bc2adb9
Simon Yi <yi.simon.k@gmail.com>
2014-12-23 17:16:29 -0500

    fixing paperclip bug to validate attachment content type in pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,5 @@ class Pin < ActiveRecord::Base
      belongs_to :user
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
      validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
+     validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png)
 end
